abee9a241a7ed1a3765cbd9307f439b0a4e38e1d
Tarun K Menon <2110080008@klh.edu.in>
2022-06-14 13:16:41 +0530

    goButton fixed and Required Attribute fixed

--- code/script.js
+++ code/script.js
@@ -1,20 +1,18 @@
 var gobtn = document.querySelector(".goButton");
 var skipbtn = document.querySelector(".skipButton");
 var content = document.getElementById("namespace");
-gobtn.onclick =()=>{
-    if(content.value == "")
-    {
-        
-    }
-    else{
-        localStorage.setItem("username", content.value);
-        window.location.href = "home.html";
-        }
-    }
-    
-skipbtn.onclick =()=>{
-        
-    localStorage.setItem("username", "Guest");
+
+gobtn.addEventListener("click", () => {
+  if (content.value.trim() == "") {
+    content.required = true;
+    document.getElementById("val").innerHTML = "Please Enter a Valid Username";
+  } else {
+    localStorage.setItem("username", content.value);
     window.location.href = "home.html";
-}
- 
+  }
+});
+
+skipbtn.addEventListener("click", () => {
+  localStorage.setItem("username", "Guest");
+  window.location.href = "home.html";
+});
